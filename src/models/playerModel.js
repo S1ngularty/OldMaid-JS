080566df3c5f50f40cd4910476f5c5a94f860de4
S1ngularty <asherxd10245@gmail.com>
2025-08-11 21:49:32 +0800

    refactor methods and services  and players pov get and offer their cards

--- src/models/playerModel.js
+++ src/models/playerModel.js
@@ -1,30 +1,53 @@
+import {
+  randDigit,
+  createPlayer,
+  initCards,
+  resetTable,
+  getCardFromDealer,
+  createCard,
+} from "../services/gameService.js";
+
 class Player {
-  #originalCard=[]
+  #originalCard = [];
   #playerCards = [];
-  #discardPiles=[]
+  #discardPiles = [];
   constructor(name) {
     this.playerName = name;
   }
 
   receiveCards(card) {
     this.#playerCards.push(card);
+    if (this.#playerCards.length >= 2) {
+      this.discardPile();
+    }
   }
 
   sortCard() {
-    this.#originalCard = [...this.#playerCards]
     this.#playerCards.sort();
   }
 
-  discardPile(){
-    for(let i =0; i<this.#playerCards.length-1; i++){
+  discardPile() {
+    this.sortCard();
+    for (let i = 0; i < this.#playerCards.length - 1; i++) {
       // console.log(this.#playerCards[i].split(" ")[0],this.#playerCards[i+1].split(" ")[0])
-      if((this.#playerCards[i].split(" ")[0]).trim()===(this.#playerCards[i+1].split(" ")[0]).trim()){
-        this.#discardPiles.push(this.#playerCards.splice(i,2))
-        i--
+      if (
+        this.#playerCards[i].split(" ")[0].trim() ===
+        this.#playerCards[i + 1].split(" ")[0].trim()
+      ) {
+        this.#discardPiles.push(this.#playerCards.splice(i, 2));
+        i--;
       }
     }
   }
 
+  offerCard() {
+    return this.#playerCards;
+  }
+
+  async getCardFromDealer() {
+    let cardFromDealer = await getCardFromDealer();
+    this.receiveCards(cardFromDealer);
+  }
 
   get cards() {
     return this.#playerCards;
